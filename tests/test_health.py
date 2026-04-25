eef90ff5d349975e4739255d7223012c33de7834
kuaruan <kuaruan.projects@gmail.com>
2026-04-24 20:52:13 -0400

    corrección final en test

--- tests/test_health.py
+++ tests/test_health.py
@@ -10,4 +10,4 @@ def test_root():
 def test_health():
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.json() == {"status": "Todo Okay!!"}
+    assert response.json() == {"status": "Todo Okay!"}
